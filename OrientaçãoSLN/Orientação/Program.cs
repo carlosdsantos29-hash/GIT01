afe125d27807c6c584f994412c3ae060bbd56fcb
CarlosDEV <carlos.d.santos29@aluno.senai.br>
2026-04-24 09:51:51 -0300

    Soma Antecessores

---
+++ OrientaçãoSLN/Orientação/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Xml;
+
+Console.WriteLine("Digite um num:");
+double num = int.Parse(Console.ReadLine());
+double soma = 0;
+for (int i = 0; i < num; i++){
+    soma += num;
+}
+Console.WriteLine($"Soma dos números antecessores ao digitado: {soma}");
